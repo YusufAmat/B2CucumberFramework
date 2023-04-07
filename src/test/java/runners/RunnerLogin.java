7ef7a833b21a0d4aa5035d97adccfb5427ccf23d
atezgider <atezgider@gmail.com>
2023-04-07 20:31:14 +0200

    2023-04-07

--- src/test/java/runners/RunnerLogin.java
+++ src/test/java/runners/RunnerLogin.java
@@ -1,22 +1,18 @@
 package runners;
 
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.BeforeTest;
 
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = {"stepdefs"},
-        tags = "@WishList"
-        /*
+        tags = "@WishList",
         plugin = { "pretty",
                 "json:test-output/cucumber-reports/cucumber.json",
                 "html:test-output/cucumber-reports/cucumberreport.html",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 
-         */
 )
 public class RunnerLogin extends AbstractTestNGCucumberTests {
 
