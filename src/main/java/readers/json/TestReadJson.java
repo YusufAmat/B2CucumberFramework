9d90f0d3bd5c35b6af64ec94c47ef43d8f1e4fcc
atezgider <atezgider@gmail.com>
2023-04-10 19:44:29 +0200

    2023-04-10

--- src/main/java/readers/json/TestReadJson.java
+++ src/main/java/readers/json/TestReadJson.java
@@ -1,4 +1,22 @@
 package readers.json;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+
 public class TestReadJson {
+
+
+    @Test
+    public void readConfig1() throws Exception{
+
+        // olunacak json dosyasi
+        String file = "src/test/resources/datafiles/Config1.json";
+
+        // Config1.json JSONbjeckt olarak okunuyor
+        JSONObject object = (JSONObject) JSONValue.parse(new FileReader(file));
+        System.out.println(object);
+    }
 }
